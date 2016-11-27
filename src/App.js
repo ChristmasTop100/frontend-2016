8c3bd6d993a9a0534bd32dbfe0da3da48d1576ee
Dylan Companjen <info@dylancompanjen.com>
2016-11-27 12:11:28 +0100

    Added Material-UI

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
 import React, { Component } from 'react';
-import Snowfetti from './components/Snowfetti';
+import AppBar from 'material-ui/AppBar';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import Background from './components/Background';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.updateSnowDimensions = this.updateSnowDimensions.bind(this)
-    this.state = {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }
-  }
-
-  updateSnowDimensions() {
-    this.setState({
-      width: window.innerWidth,
-      height: window.innerHeight
-    })
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", this.updateSnowDimensions)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateSnowDimensions)
-  }
-
   render() {
     return (
       <div className="App">
-        <Snowfetti
-          type="snow"
-          profile={[ 'snow', 'fast' ]}
-          amount={800}
-          width={this.state.width}
-          height={this.state.height}
-          styles={{
-            backgroundColor: '#000',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            zIndex: -1
-          }}
+        <Background />
+        <AppBar
+          title="Christmas Top 100"
+          showMenuIconButton={false}
+          style={{backgroundColor: 'rgba(213, 0, 0, 0.9)'}}
         />
-        <div className="App-header">
-          <h2>Christmas Top 100</h2>
+        <div style={{maxWidth: '1100px', margin: '40px auto', padding: '0 15px'}}>
+          <Table style={{backgroundColor: 'rgba(255,255,255,0.9)'}}>
+            <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+              <TableRow>
+                <TableHeaderColumn>ID</TableHeaderColumn>
+                <TableHeaderColumn>Name</TableHeaderColumn>
+                <TableHeaderColumn>Status</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+              <TableRow>
+                <TableRowColumn>1</TableRowColumn>
+                <TableRowColumn>John Smith</TableRowColumn>
+                <TableRowColumn>Employed</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>2</TableRowColumn>
+                <TableRowColumn>Randal White</TableRowColumn>
+                <TableRowColumn>Unemployed</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>3</TableRowColumn>
+                <TableRowColumn>Stephanie Sanders</TableRowColumn>
+                <TableRowColumn>Employed</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>4</TableRowColumn>
+                <TableRowColumn>Steve Brown</TableRowColumn>
+                <TableRowColumn>Employed</TableRowColumn>
+              </TableRow>
+            </TableBody>
+          </Table>
         </div>
       </div>
     );
---
+++ src/components/Background/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Snowfetti from '../Snowfetti';
+
+class Background extends Component {
+  constructor() {
+    super()
+    this.updateSnowDimensions = this.updateSnowDimensions.bind(this)
+    this.state = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+  }
+
+  updateSnowDimensions() {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateSnowDimensions)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateSnowDimensions)
+  }
+
+  render() {
+    return (
+      <Snowfetti
+        type="snow"
+        profile={[ 'snow', 'fast' ]}
+        amount={800}
+        width={this.state.width}
+        height={this.state.height}
+        styles={{
+          backgroundColor: '#212121',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          zIndex: -1
+        }}
+      />
+    );
+  }
+}
+
+export default Background;
