8f65702f2aafc73eeed6b094aee22b87aa8804d9
Dylan Companjen <info@dylancompanjen.com>
2016-11-28 08:46:04 +0100

    Cleaned up the inline styles

--- src/containers/SongsList/SongsList.js
+++ src/containers/SongsList/SongsList.js
@@ -1,6 +1,59 @@
 import React, { Component } from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
 
+const styles = {
+  table: {
+    backgroundColor: 'rgba(12,12,12,0.95)',
+    width: '100%',
+    color: 'white',
+    borderCollapse: 'collapse'
+  },
+  thead: {
+    letterSpacing: '4px',
+    color: '#3a3e53',
+    fontWeight: 700,
+    fontSize: '12px',
+    textAlign: 'left'
+  },
+  tr: {
+    borderBottom: '1px solid rgba(255,255,255,0.025)',
+    lineHeight: 0.6
+  },
+  indexCol: {
+    width: '100px'
+  },
+  songCol: {
+    padding: '15px 0'
+  },
+  index: {
+    fontSize: '30px',
+    fontWeight: '300',
+    color: 'rgba(255,255,255,0.7)',
+    padding: '15px 24px'
+  },
+  imageHolder: {
+    padding: '15px 0'
+  },
+  image: {
+    display: 'block',
+    width: '80px',
+    height: '80px'
+  },
+  song: {
+    lineHeight: 1.3,
+    fontSize: '24px',
+    padding: '0 24px'
+  },
+  artist: {
+    fontSize: '16px',
+    fontWeight: 300,
+    color: 'rgba(255,255,255,0.7)'
+  },
+  spotifyPlayer: {
+    display: 'block'
+  }
+}
+
 class SongsList extends Component {
   constructor() {
     super()
@@ -46,31 +99,28 @@ class SongsList extends Component {
     }
     return (
       <div>
-        <table style={{backgroundColor: 'rgba(12,12,12,0.95)', width: '100%', color: 'white', borderCollapse: 'collapse'}}>
-          <thead style={{letterSpacing: '4px', color: '#3a3e53', fontWeight: 700, fontSize: '12px', textAlign: 'left'}}>
-            <tr style={{borderBottom: '1px solid rgba(255,255,255,0.025)'}}>
-              <th style={{height: '50px', width: '100px'}}></th>
-              <th style={{height: '50px', width: '80px'}}>SONG</th>
-              <th style={{height: '50px'}}></th>
-              <th style={{height: '50px'}}>VOTES</th>
-              <th style={{height: '50px'}}>PLAY</th>
+        <table style={styles.table}>
+          <thead style={styles.thead}>
+            <tr style={styles.tr}>
+              <th style={styles.indexCol}></th>
+              <th style={styles.songCol}>SONG</th>
+              <th></th>
+              <th>VOTES</th>
+              <th>PLAY</th>
             </tr>
           </thead>
           <tbody>
             {this.state.data.map((song, index) => {
               const trackId = song.url.replace('https://open.spotify.com/track/', '')
               return (
-                <tr key={`song-${index}`} style={{borderBottom: '1px solid rgba(255,255,255,0.025)'}}>
-                  <td style={{
-                    fontSize: '30px',
-                    fontWeight: '300',
-                    color: 'rgba(255,255,255,0.7)',
-                    padding: '15px 24px'
-                  }}>{index+1}</td>
-                  <td style={{padding: '15px 0'}}><img style={{display: 'block'}} src={song.image} width="80" height="80" alt="" /></td>
-                  <td style={{lineHeight: 1.3, fontSize: '24px', padding: '0 24px'}}>
+                <tr key={`song-${index}`} style={styles.tr}>
+                  <td style={styles.index}>{index+1}</td>
+                  <td style={styles.imageHolder}>
+                    <img style={styles.image} src={song.image} alt="" />
+                  </td>
+                  <td style={styles.song}>
                     <div>{song.title}</div>
-                    <div style={{fontSize: '16px', fontWeight: 300, color: 'rgba(255,255,255,0.7)'}}>{song.artist}</div>
+                    <div style={styles.artist}>{song.artist}</div>
                   </td>
                   <td></td>
                   <td>
@@ -80,7 +130,7 @@ class SongsList extends Component {
                       height="80"
                       frameBorder="0"
                       allowTransparency="true"
-                      style={{display: 'block'}}
+                      style={styles.spotifyPlayer}
                     />
                   </td>
                 </tr>
