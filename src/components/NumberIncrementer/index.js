e080c6b28868104f568d586f2bf5297826f887d4
Dylan Companjen <info@dylancompanjen.com>
2016-12-04 19:28:12 +0100

    Finished the interface for the number incrementer

--- src/components/NumberIncrementer/index.js
+++ src/components/NumberIncrementer/index.js
@@ -3,9 +3,9 @@ import RaisedButton from 'material-ui/RaisedButton';
 import './NumberIncrementer.css'
 
 const buttonStyle = {
-  height: 24,
+  height: 15,
   width: 24,
-  lineHeight: '24px',
+  lineHeight: '15px',
   color: 'white'
 };
 
@@ -13,27 +13,29 @@ class NumberIncrementer extends Component {
   render() {
     return (
       <div className="NumberIncrementer">
-        <RaisedButton
-          buttonStyle={buttonStyle}
-          labelStyle={buttonStyle}
-          overlayStyle={buttonStyle}
-          primary={true}
-          className="Min"
-          onClick={this.props.minClick}
-        >
-          -
-        </RaisedButton>
         <div className="Value">{this.props.value}</div>
-        <RaisedButton
-          buttonStyle={buttonStyle}
-          labelStyle={buttonStyle}
-          overlayStyle={buttonStyle}
-          primary={true}
-          className="Plus"
-          onClick={this.props.plusClick}
-        >
-          +
-        </RaisedButton>
+        <div className="Buttons">
+          <RaisedButton
+            buttonStyle={buttonStyle}
+            labelStyle={buttonStyle}
+            overlayStyle={buttonStyle}
+            primary={true}
+            className="Plus"
+            onClick={this.props.plusClick}
+          >
+            +
+          </RaisedButton>
+          <RaisedButton
+            buttonStyle={buttonStyle}
+            labelStyle={buttonStyle}
+            overlayStyle={buttonStyle}
+            primary={true}
+            className="Min"
+            onClick={this.props.minClick}
+          >
+            -
+          </RaisedButton>
+        </div>
       </div>
     );
   }
