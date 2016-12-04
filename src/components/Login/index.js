565ab88156820218ccc2539bf0e51af9ddbe2e66
Dylan Companjen <info@dylancompanjen.com>
2016-12-04 17:17:45 +0100

    Added basic login screen

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import './Login.css'
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        Login view
+      <div className="Login">
+        <TextField
+          floatingLabelText="Username"
+          fullWidth={true}
+        />
+        <TextField
+          floatingLabelText="Password"
+          fullWidth={true}
+        />
+        <RaisedButton
+          className="Submit"
+          label="Login"
+          primary={true}
+          fullWidth={true}
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router'
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import {redA400} from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import routes from './routes'
 import './index.css';
 
@@ -10,9 +12,19 @@ import './index.css';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-ReactDOM.render(
-  <MuiThemeProvider>
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: redA400
+  }
+})
+
+const AppProvider = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
     <Router history={browserHistory} routes={routes} />
-  </MuiThemeProvider>,
+  </MuiThemeProvider>
+)
+
+ReactDOM.render(
+  <AppProvider />,
   document.getElementById('root')
 );
