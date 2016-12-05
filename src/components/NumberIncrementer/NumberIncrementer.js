7840d07de71d6daf74c0a00974584964f3be8497
Dylan Companjen <dylan.companjen@oneshoe.nl>
2016-12-05 20:21:15 +0100

    Updated the performance of the incrementer button

--- src/components/NumberIncrementer/NumberIncrementer.js
+++ src/components/NumberIncrementer/NumberIncrementer.js
@@ -5,8 +5,18 @@ const NumberIncrementer = ({ minClick, plusClick, value }) => (
   <div className="NumberIncrementer">
     <div className="Value">{value}</div>
     <div className="Buttons">
-      <button onClick={plusClick}>+</button>
-      <button onClick={minClick}>-</button>
+      <button
+        onClick={plusClick}
+        className={value === 20 ? 'disabled' : ''}
+      >
+        +
+      </button>
+      <button
+        onClick={minClick}
+        className={value === 0 ? 'disabled' : ''}
+      >
+        -
+      </button>
     </div>
   </div>
 )
