b5b416a472feb2a980a2d35db9e176bc018eeb93
Yaron Tal <yaron.tal@oneshoe.nl>
2016-12-11 19:24:04 +0100

    added helper function to process one time logins

--- src/utils/XmasAPI.js
+++ src/utils/XmasAPI.js
@@ -11,18 +11,39 @@ class XmasAPI {
         }
       }
     }`
-    return fetch(`https://back.christmastop100.nl/graphql?query=${query}`)
-      .then(response => response.json())
+    return this.query(query)
   }
 
   static loginUser(username, password) {
-    const query = `{
+    const query = `mutation {
       CreateSession(
         email:"${username}",
         password:"${password}"
       ) { token }
     }`
-    return fetch(`https://back.christmastop100.nl/graphql?query=mutation${query}`)
+    return this.query(query)
+  }
+
+  static oneTimeLogin(email, password, token) {
+    const query = `mutation {
+      UpdateUser(
+        password:"${password}",
+        email:"${email}",
+        token:"${token}"
+      ) {
+        name,
+        id
+      }
+    }`
+
+    return this.query(query)
+  }
+
+  static query(query) {
+    return fetch(`https://back.christmastop100.nl/graphql?query=${query}`, {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    })
       .then(response => response.json())
   }
 }
